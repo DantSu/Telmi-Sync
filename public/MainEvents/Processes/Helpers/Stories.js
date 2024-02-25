7d634e43dfa70707a2a7b0bb7ac394fd9234b552
Franck ALARY <dantsu@hotmail.com>
2024-02-25 06:13:20 +0100

    fix age issue

--- public/MainEvents/Processes/Helpers/Stories.js
+++ public/MainEvents/Processes/Helpers/Stories.js
@@ -7,15 +7,15 @@ const
   findAgeInStoryName = (title) => {
     const a = title.match(/^([0-9]{1,2})\+](.*)/)
     if (a !== null) {
-      return {age: parseInt(a[1], 10), title: a[2]}
+      return {age: parseInt(a[1], 10), title: a[2].trim()}
     }
     const b = title.match(/^\[([0-9]{1,2})\+](.*)/)
     if (b !== null) {
-      return {age: parseInt(a[1], 10), title: b[2]}
+      return {age: parseInt(a[1], 10), title: b[2].trim()}
     }
     const c = title.match(/(.*)\[([0-9]{1,2})\+]$/)
     if (c !== null) {
-      return {age: parseInt(a[2], 10), title: c[1]}
+      return {age: parseInt(a[2], 10), title: c[1].trim()}
     }
     return {title}
   }
