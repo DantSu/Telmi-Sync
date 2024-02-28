944f144341e1b3565c97e773e5f1737cbc945acb
Franck ALARY <dantsu@hotmail.com>
2024-02-28 23:08:49 +0100

    linux eject shell

--- extraResources/eject/linux/eject.sh
+++ extraResources/eject/linux/eject.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mountPart=$(df "$1" | tail -1 | awk '{ print $1 }')
 umount -l "$1"
+mountPart=$(df "$1" | tail -1 | awk '{ print $1 }')
 udisksctl power-off -b "$mountPart" --no-user-interaction
