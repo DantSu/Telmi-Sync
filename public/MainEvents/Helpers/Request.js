75005a0f84fffda16a694f8f6db90cc77dfd0507
Franck ALARY <dantsu@hotmail.com>
2024-03-25 12:21:01 +0100

    Fix download large file crash by chunking request

--- public/MainEvents/Helpers/Request.js
+++ public/MainEvents/Helpers/Request.js
@@ -7,82 +7,125 @@ const
     'Accept-Language': 'en-US,en;q=0.5',
     'Connection': 'keep-alive',
     'language': 'fr',
-    'User-Agent': 'TelmiSync/0.0.0 ( https://github.com/DantSu/Telmi-Sync )',
+    'User-Agent': 'TelmiSync/0.1.1 ( https://github.com/DantSu/Telmi-Sync )',
   },
 
   htmlTag = '<!DOCTYPE html>',
 
-  downloadFile = (url, filePath, onProgress) => {
-    return new Promise((resolve, reject) => {
-      try {
-        const
-          file = fs.createWriteStream(filePath),
-          req = request(
-            url,
-            {
-              method: 'GET',
-              protocol: 'https:',
-              port: 443,
-              rejectUnauthorized: false,
-              headers: {...defaultHeader}
-            },
-            res => {
-              if (res.statusCode < 200 || res.statusCode >= 400) {
+  downloadRangeFile = (url, filePath, resolve, reject, onProgress, bytesStart, bytesLength) => {
+    if (bytesStart !== undefined && bytesStart > bytesLength) {
+      return resolve(filePath)
+    }
+
+    try {
+      const
+        bytesEnd = bytesStart !== undefined ? (bytesStart + 268435456) : undefined,
+        file = fs.createWriteStream(filePath, {flags: 'a'}),
+        req = request(
+          url,
+          {
+            method: 'GET',
+            protocol: 'https:',
+            port: 443,
+            rejectUnauthorized: false,
+            headers: Object.assign(
+              {...defaultHeader},
+              bytesStart !== undefined ? {'Range': 'bytes=' + bytesStart + '-' + (bytesEnd - 1)} : null
+            )
+          },
+          (res) => {
+            if (res.statusCode < 200 || res.statusCode >= 400) {
+              file.close()
+              return reject(new Error('statusCode=' + res.statusCode))
+            }
+
+            if (res.statusCode >= 300 && res.statusCode < 400) {
+              if (typeof res.headers['location'] === 'string' && res.headers['location'] !== url) {
+                file.close()
+                return downloadFile(res.headers['location'], filePath, onProgress)
+                  .then((filePath) => resolve(filePath))
+                  .catch((e) => reject(e))
+              } else {
                 file.close()
                 return reject(new Error('statusCode=' + res.statusCode))
               }
-
-              if (res.statusCode >= 300 && res.statusCode < 400) {
-                if (typeof res.headers['location'] === 'string' && res.headers['location'] !== url) {
-                  file.close()
-                  return downloadFile(res.headers['location'], filePath, onProgress)
-                    .then((filePath) => resolve(filePath))
-                    .catch((e) => reject(e))
-                } else {
-                  file.close()
-                  return reject(new Error('statusCode=' + res.statusCode))
-                }
-              }
-
-              const fileLength = res.headers['content-length']
-              let fileProgress = 0
-              res.on('data', (d) => {
-                fileProgress += d.length
-                onProgress(Math.round(fileProgress / (fileLength || fileProgress) * 100), 100)
-              })
-
-              res.pipe(file)
-              file.on('finish', () => {
-                file.close()
-                if (fileProgress > 10000) {
-                  return resolve(filePath)
-                }
-                const content = fs.readFileSync(filePath).toString('utf8')
-                if (content.substring(0, htmlTag.length) !== htmlTag) {
-                  return resolve(filePath)
-                }
-                const startPos = content.toLowerCase().indexOf(' url=') + 5
-                if (startPos === -1) {
-                  return resolve(filePath)
-                }
-                const endPos = content.indexOf('"', startPos)
-                if (endPos === -1) {
-                  return resolve(filePath)
-                }
-                return downloadFile(content.substring(startPos, endPos), filePath, onProgress)
-                  .then((filePath) => resolve(filePath))
-                  .catch((e) => reject(e))
-              })
             }
-          )
-        req.on('error', e => {
-          file.close()
-          reject(e)
-        })
-        req.end()
-      } catch (e) {
+
+            let fileProgress = bytesStart || 0
+            res.on('data', (d) => {
+              fileProgress += d.length
+              onProgress(Math.round(fileProgress / (bytesLength || fileProgress) * 100), 100)
+            })
+
+            res.pipe(file)
+            file.on('finish', () => {
+              file.close()
+              if (fileProgress > 512000) {
+                return downloadRangeFile(url, filePath, resolve, reject, onProgress, bytesEnd || 2, bytesLength || 1)
+              }
+              const content = fs.readFileSync(filePath).toString('utf8')
+              if (content.substring(0, htmlTag.length) !== htmlTag) {
+                return downloadRangeFile(url, filePath, resolve, reject, onProgress, bytesEnd || 2, bytesLength || 1)
+              }
+              const startPos = content.toLowerCase().indexOf(' url=') + 5
+              if (startPos === -1) {
+                return downloadRangeFile(url, filePath, resolve, reject, onProgress, bytesEnd || 2, bytesLength || 1)
+              }
+              const endPos = content.indexOf('"', startPos)
+              if (endPos === -1) {
+                return downloadRangeFile(url, filePath, resolve, reject, onProgress, bytesEnd || 2, bytesLength || 1)
+              }
+              return downloadFile(content.substring(startPos, endPos), filePath, onProgress)
+                .then((filePath) => resolve(filePath))
+                .catch((e) => reject(e))
+            })
+          }
+        )
+      req.on('error', e => {
+        file.close()
         reject(e)
+      })
+      req.end()
+    } catch (e) {
+      reject(e)
+    }
+  },
+
+  downloadFile = (url, filePath, onProgress) => {
+    return new Promise((resolve, reject) => {
+      if (fs.existsSync(filePath)) {
+        fs.rmSync(filePath)
       }
+
+      const req = request(
+        url,
+        {method: 'HEAD'},
+        (res) => {
+          if (res.statusCode < 200 || res.statusCode >= 400) {
+            return reject(new Error('statusCode=' + res.statusCode))
+          }
+
+          if (res.statusCode >= 300 && res.statusCode < 400) {
+            if (typeof res.headers['location'] === 'string' && res.headers['location'] !== url) {
+              return downloadFile(res.headers['location'], filePath, onProgress)
+                .then((filePath) => resolve(filePath))
+                .catch((e) => reject(e))
+            } else {
+              return reject(new Error('statusCode=' + res.statusCode))
+            }
+          }
+
+          if(res.headers['accept-ranges'] === 'bytes' && res.headers['content-length'] !== undefined) {
+            downloadRangeFile(url, filePath, resolve, reject, onProgress, 0, res.headers['content-length'])
+          } else {
+            downloadRangeFile(url, filePath, resolve, reject, onProgress)
+          }
+        }
+      )
+      req.on('error', e => {
+        reject(e)
+      })
+      req.end()
     })
   },
 
--- public/MainEvents/Processes/Store/StoreDownload.js
+++ public/MainEvents/Processes/Store/StoreDownload.js
@@ -1,41 +1,8 @@
 import { downloadFile } from '../../Helpers/Request.js'
-import { getBinPath, initTmpPath } from '../Helpers/AppPaths.js'
-import { rmFile } from '../../Helpers/Files.js'
-import * as fs from 'fs'
+import { initTmpPath } from '../Helpers/AppPaths.js'
 import { getProcessParams } from '../Helpers/ProcessParams.js'
 import * as path from 'path'
 import convertZip from '../Import/ConvertZip.js'
-
-const
-  links = [
-    'https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffmpeg-6.1-win-64.zip',
-    'https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffmpeg-6.1-linux-64.zip',
-    'https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffmpeg-6.1-linux-arm-64.zip',
-    'https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffmpeg-6.1-macos-64.zip'
-  ],
-  baseUrl = 'https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffmpeg-6.1-',
-  getOS = () => {
-    switch (process.platform) {
-      case 'darwin':
-        return 'macos'
-      case 'linux':
-        return 'linux'
-      case 'win32':
-        return 'win'
-      default:
-        return null
-    }
-  },
-  getArch = () => {
-    switch (process.arch) {
-      case 'x64':
-        return '64'
-      case 'arm64':
-        return 'arm-64'
-      default:
-        return null
-    }
-  }
 
 function main (url) {
 
