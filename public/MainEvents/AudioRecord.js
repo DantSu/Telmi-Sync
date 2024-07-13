e6eda9fd2ae85a4cc8fe143bd606e4cbd866a04a
Franck Alary <franck@alary.pro>
2024-07-13 15:07:20 +0200

    Studio WIP : audio record macos

--- public/MainEvents/AudioRecord.js
+++ public/MainEvents/AudioRecord.js
@@ -1,9 +1,28 @@
-import {ipcMain} from 'electron'
+import {ipcMain, systemPreferences} from 'electron'
 import * as path from 'path'
 import fs from 'fs'
 import {initTmpPath} from './Helpers/AppPaths.js'
 
 function mainEventAudioRecord(mainWindow) {
+
+  ipcMain.on(
+    'audio-request-mic',
+    async () => {
+      try {
+        if (systemPreferences.getMediaAccessStatus('microphone') === 'granted') {
+          mainWindow.webContents.send('audio-request-mic-result', true)
+          return
+        }
+        mainWindow.webContents.send(
+          'audio-request-mic-result',
+          await systemPreferences.askForMediaAccess('microphone')
+        )
+      } catch (e) {
+        mainWindow.webContents.send('audio-request-mic-result', true)
+      }
+    }
+  )
+
   ipcMain.on(
     'audio-record-buffer-to-file',
     async (event, arrayBuffer) => {
--- src/App/Components/Audio/AudioRecord.js
+++ src/App/Components/Audio/AudioRecord.js
@@ -28,50 +28,10 @@ function AudioRecord({title, onRecordEnded, className}) {
           }
         } else {
           setIsRecording(true)
-          navigator.mediaDevices.getUserMedia({
-            audio: typeof params.microphone === 'string' ? {deviceId: params.microphone} : true,
-            video: false
-          })
-            .then(stream => {
-              const
-                track = stream.getAudioTracks()[0],
-                capabilities = track.getCapabilities()
-              track
-                .applyConstraints(Object.assign(
-                  {},
-                  capabilities.autoGainControl.includes(true) ? {autoGainControl: true} : null,
-                  capabilities.channelCount.min <= 2 && capabilities.channelCount.max >= 2 ? {channelCount: 2} : null,
-                  capabilities.echoCancellation.includes(true) ? {echoCancellation: true} : null,
-                  capabilities.noiseSuppression.includes(true) ? {noiseSuppression: true} : null,
-                  capabilities.sampleRate.min <= 44100 && capabilities.sampleRate.max >= 44100 ? {sampleRate: 44100} : null,
-                  capabilities.sampleSize.min <= 16 && capabilities.sampleSize.max >= 16 ? {sampleSize: 16} : null,
-                ))
-                .then(() => {
-                  const mr = new MediaRecorder(stream)
-                  mr.start()
-                  let chunks = []
-                  mr.ondataavailable = (e) => {
-                    chunks.push(e.data)
-                  }
-                  mr.onstop = () => {
-                    (new Blob(chunks, {type: 'audio/webm'}))
-                      .arrayBuffer()
-                      .then((arr) => ipcRenderer.send('audio-record-buffer-to-file', arr))
-                  }
-                  setMediaRecorder(mr)
-                })
-                .catch(error => {
-                  console.log('Error :', error)
-                  setIsRecording(false)
-                })
-            })
-            .catch(error => {
-              console.log('Error :', error)
-              setIsRecording(false)
-            })
+          ipcRenderer.send('audio-request-mic')
         }
       },
-      [isRecording, mediaRecorder, params.microphone]
+      [isRecording, mediaRecorder]
     )
 
 
@@ -83,6 +43,60 @@ function AudioRecord({title, onRecordEnded, className}) {
       }
     },
     [mediaRecorder]
+  )
+
+  useElectronListener(
+    'audio-request-mic-result',
+    (accessGranted) => {
+      if (!accessGranted) {
+        console.log('Error : Access to microphone declined !')
+        setIsRecording(false)
+        return
+      }
+
+      navigator.mediaDevices.getUserMedia({
+        audio: typeof params.microphone === 'string' ? {deviceId: params.microphone} : true,
+        video: false
+      })
+        .then(stream => {
+          const
+            track = stream.getAudioTracks()[0],
+            capabilities = track.getCapabilities()
+          track
+            .applyConstraints(Object.assign(
+              {},
+              capabilities.autoGainControl.includes(true) ? {autoGainControl: true} : null,
+              capabilities.channelCount.min <= 2 && capabilities.channelCount.max >= 2 ? {channelCount: 2} : null,
+              capabilities.echoCancellation.includes(true) ? {echoCancellation: true} : null,
+              capabilities.noiseSuppression.includes(true) ? {noiseSuppression: true} : null,
+              capabilities.sampleRate.min <= 44100 && capabilities.sampleRate.max >= 44100 ? {sampleRate: 44100} : null,
+              capabilities.sampleSize.min <= 16 && capabilities.sampleSize.max >= 16 ? {sampleSize: 16} : null,
+            ))
+            .then(() => {
+              const mr = new MediaRecorder(stream)
+              mr.start()
+              let chunks = []
+              mr.ondataavailable = (e) => {
+                chunks.push(e.data)
+              }
+              mr.onstop = () => {
+                (new Blob(chunks, {type: 'audio/webm'}))
+                  .arrayBuffer()
+                  .then((arr) => ipcRenderer.send('audio-record-buffer-to-file', arr))
+              }
+              setMediaRecorder(mr)
+            })
+            .catch(error => {
+              console.log('Error :', error)
+              setIsRecording(false)
+            })
+        })
+        .catch(error => {
+          console.log('Error :', error)
+          setIsRecording(false)
+        })
+    },
+    [params.microphone]
   )
 
   useElectronListener(
