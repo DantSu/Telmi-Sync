89fc7bab6ec3fd1b00f573fec33df5fb2ae5ca06
Franck Alary <franck@alary.pro>
2024-09-26 14:06:52 +0200

    Github Workflow rollback node 20

--- src/App/Modules/Studio/StudioContainer.js
+++ src/App/Modules/Studio/StudioContainer.js
@@ -1,11 +1,11 @@
-import StudioStoriesTable from './Stories/StoriesTable.js'
 import {useState} from 'react'
+import StudioStoriesTable from './Stories/StoriesTable.js'
 import StudioStoryEditor from './Editor/StudioStoryEditor.js'
 
-function StudioContainer() {
-  const [story, setStory] = useState(null)
-  if(story === null) {
-  return <StudioStoriesTable setStory={setStory}/>
+function StudioContainer({openedStory}) {
+  const [story, setStory] = useState(openedStory || null)
+  if (story === null) {
+    return <StudioStoriesTable setStory={setStory}/>
   } else {
     return <StudioStoryEditor story={story} setStory={setStory}/>
   }
