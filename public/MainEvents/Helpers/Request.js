b0387bb466a07ebca1780112d522e3942a99d0ee
Franck Alary <franck@alary.pro>
2024-12-04 06:56:53 +0100

    v0.7.0

--- public/MainEvents/Helpers/Request.js
+++ public/MainEvents/Helpers/Request.js
@@ -9,7 +9,7 @@ const
     'Accept-Language': 'en-US,en;q=0.5',
     'Connection': 'keep-alive',
     'language': 'fr',
-    'User-Agent': 'TelmiSync/0.6.1 ( https://github.com/DantSu/Telmi-Sync )',
+    'User-Agent': 'TelmiSync/0.7.0 ( https://github.com/DantSu/Telmi-Sync )',
   },
 
   htmlTag = '<!DOCTYPE html>',
