065280c4bb0014d29fc8242cd4d9bcb573109887
Franck Alary <franck@alary.pro>
2025-07-15 11:15:43 +0200

    Telmi OS SD card maker : MacOS and linux WIP

--- extraResources/fat32/darwin/format.sh
+++ extraResources/fat32/darwin/format.sh
@@ -5,23 +5,23 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-VOLUME_NAME="$1"
-
-DEVICE=$(df "/Volumes/$VOLUME_NAME" 2>/dev/null | tail -1 | awk '{print $1}')
+MOUNT_PATH="$1"
+VOLUME_NAME=$(basename "$MOUNT_PATH")
+DEVICE=$(df "$MOUNT_PATH" 2>/dev/null | tail -1 | awk '{print $1}')
 
 if [ -z "$DEVICE" ]; then
   echo "wrong-parameters" >&2
   exit 1
 fi
 
-DISK=$(echo "$PART_DEVICE" | sed -E 's|/dev/(disk[0-9]+)s[0-9]+|\1|')
+DISK=$(echo "$DEVICE" | sed -E 's|/dev/(disk[0-9]+)s[0-9]+|\1|')
 
 if ! diskutil unmountDisk "/dev/$DISK" >/dev/null 2>&1; then
   echo "formatting-failed" >&2
   exit 1
 fi
 
-if ! diskutil eraseDisk FAT32 "Telmi OS" MBRFormat "/dev/$DISK" >/dev/null; then
+if ! diskutil eraseDisk FAT32 "$VOLUME_NAME" MBRFormat "/dev/$DISK" >/dev/null; then
   echo "formatting-failed" >&2
   exit 1
 fi
