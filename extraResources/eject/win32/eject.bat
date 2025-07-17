2946190b7652eeef31ae83809f65d52183faf1af
Franck Alary <franck@alary.pro>
2025-07-17 15:12:28 +0200

    Fix eject sd card on windows

--- extraResources/eject/win32/eject.bat
+++ extraResources/eject/win32/eject.bat
@@ -1,3 +1,8 @@
 chcp 65001
-set dir=%~dp0
-call "%dir%eject.exe" "%1" -L
+
+set DRIVE=%~1
+set BAT_PATH=%~dp0
+
+powershell -Command "(New-Object -comObject Shell.Application).Namespace(17).ParseName('%DRIVE%').InvokeVerb('Eject'); Start-Sleep -Milliseconds 1500"
+
+exit /b 0
--- public/MainEvents/Processes/BinFiles/EjectCommand.js
+++ public/MainEvents/Processes/BinFiles/EjectCommand.js
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process'
+import {spawn} from 'child_process'
 import {getExtraResourcesPath} from '../Helpers/AppPaths.js'
 import * as path from 'path'
 
@@ -26,4 +26,4 @@ const
     })
   }
 
-export { ejectDrive }
+export {ejectDrive}
